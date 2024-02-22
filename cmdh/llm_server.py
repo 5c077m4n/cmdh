d27469389357a83183382369dbff7bef78eb0a7c
Roee Shapira <35409124+5c077m4n@users.noreply.github.com>
2024-02-23 01:03:12 +0200

    Add model pull

--- cmdh/llm_server.py
+++ cmdh/llm_server.py
@@ -1,18 +1,17 @@
-import os
-import subprocess
+import time
+from subprocess import DEVNULL, Popen
 
 
 class OllamaServer:
     url = "http://localhost:11435"
 
     def __init__(self):
-        env = os.environ.copy()
-        env["OLLAMA_HOST"] = OllamaServer.url
-
-        self.ollama_serve_proc = subprocess.Popen(["ollama", "serve"], env=env)
-        self.ollama_serve_proc.communicate()
+        self.ollama_serve_proc = Popen(
+            ["ollama", "serve"], stdout=DEVNULL, stderr=DEVNULL
+        )
 
     def __enter__(self):
+        time.sleep(0.75)
         return self.ollama_serve_proc
 
     def __exit__(self, *_):
--- cmdh/main.py
+++ cmdh/main.py
@@ -1,14 +1,15 @@
 from langchain_community.llms.ollama import Ollama
+from ollama import pull
 
 from cmdh.llm_server import OllamaServer
-from cmdh.pull import fetch_model
 
 
 def main():
     with OllamaServer():
-        fetch_model()
-        client = Ollama(base_url=OllamaServer.url)
-        print(client("How are you?"))
+        pull("mistral")
+
+        client = Ollama(model="mistral")
+        print(client.invoke("How are you?"))
 
 
 if __name__ == "__main__":
